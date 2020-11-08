79176dcf038facf25bd4cb001debd4d84a32a2a0
nwinans <nwinans15@gmail.com>
2020-11-07 23:00:40 -0500

    more test cases

--- graphs/highroad_lowroad_tester.py
+++ graphs/highroad_lowroad_tester.py
@@ -2,8 +2,7 @@ from graph_util import *
 from highroad_lowroad import *
 import os
 
-tests = ['mountain.txt', 'branches.txt', 'test1.txt', 'test2.txt', 'exponential_length5.txt', 'exponential_length10.txt', 'exponential_length30.txt', 'plateau.txt', 'four_layers10.txt', 'four_layers50.txt']
-#tests = ['test1.txt']
+tests = ['test1.txt', 'test2.txt', 'exponential_length5.txt', 'exponential_length10.txt', 'exponential_length30.txt', 'plateau.txt', 'four_layers10.txt', 'four_layers50.txt', 'mountain.txt', 'branches.txt', 'switchOrder.txt', 'staircase.txt', 'falsePath.txt', 'chokePoints.txt', 'bumpyPlateau.txt']
 
 def read_input(filename):
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
