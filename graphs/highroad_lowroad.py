6c28e51cf7b3c22c45d3a18b8a7b10fce3865fb1
nwinans <nwinans15@gmail.com>
2020-11-07 22:56:09 -0500

    solved high and low road

--- graphs/highroad_lowroad.py
+++ graphs/highroad_lowroad.py
@@ -1,3 +1,4 @@
+from typing import NamedTuple
 from graph_util import *
 from math import inf
 import queue
@@ -9,83 +10,88 @@ def shortest(roadmap, elevations, start, end):
     
     We basically want to perform breadth-first search to find the shortest.
     """
+    return algorithm(roadmap, elevations, start, end)[0]
 
-    low_shortest_visited = { key:inf for key in roadmap.get_nodes() }
-    high_shortest_visited = { key:inf for key in roadmap.get_nodes() }
-
-    low_shortest_visited[start] = 0
-    high_shortest_visited[start] = 0
-
-    matches = []
-
-    q = queue.Queue()
-    q.put((start, start))
-
-    while not q.empty():
-        # grab the top pair on the stack
-        low, high = q.get()
-        #print(f"(low, high) = ({low}, {high})")
-        # are we at the end?, we want to reach the end at the same time so break if one is there
-        if low == end and high == end:
-            print(f'Other method - Apparently at end -- high {high_shortest_visited[high]}, low {low_shortest_visited[low]}')
-            continue
-
-        high_current = high_shortest_visited[high]
-        low_current = low_shortest_visited[low]
-
-        # can we move the high node?
-        for adj in roadmap.get_neighbors(high):
-            # is it a valid move?
-            if elevations[adj] > elevations[low]:
-                # have we solved this / does it make sense to continue
-                if high_current + 1 < high_shortest_visited[adj]:
-                    high_shortest_visited[adj] = high_current + 1
-                    q.put((low, adj))
-            elif adj == low and adj == end:
-                print(f'High moved - Apparently at end -- high {high_shortest_visited[high]}, low {low_shortest_visited[low]}')
-                high_shortest_visited[adj] = min(high_current + 1, high_shortest_visited[adj])
-                matches.append(high_current + 1)
-
-        # can we move the low node?
-        for adj in roadmap.get_neighbors(low):
-            # is it a valid move?
-            if elevations[adj] < elevations[high]:
-                # have we solved this / does it make sense to continue
-                if low_current + 1 < low_shortest_visited[adj]:
-                    low_shortest_visited[adj] = low_current + 1
-                    q.put((adj, high))
-            elif adj == high and adj == end:
-                print(f'Low moved - Apparently at end -- high {high_shortest_visited[high]}, low {low_shortest_visited[low]}')
-                low_shortest_visited[adj] = min(low_current + 1, low_shortest_visited[adj])
-                matches.append(low_current + 1)
-
-
-        # can we move both low and high?
-        for adj_high in roadmap.get_neighbors(high):
-            for adj_low in roadmap.get_neighbors(low):
-                if elevations[adj_high] > elevations[adj_low]:
-                    if low_current + 1 < low_shortest_visited[adj_low] or high_current + 1 < high_shortest_visited[adj_high]:
-                        low_shortest_visited[adj_low] = min(low_current + 1, low_shortest_visited[adj_low])
-                        high_shortest_visited[adj_high] = min(high_current + 1, high_shortest_visited[adj_high])
-                        q.put((adj_low, adj_high))
-                elif adj_high == adj_low and adj_high == end:
-                    print(f'Both moved - Apparently at end -- high {high_shortest_visited[high]}, low {low_shortest_visited[low]}')
-                    low_shortest_visited[adj_low] = min(low_current + 1, low_shortest_visited[adj_low])
-                    high_shortest_visited[adj_high] = min(high_current + 1, high_shortest_visited[adj_high])
-                    matches.append(max(low_current + 1, high_current + 1))
-
-    print(matches)
-
-    return max(high_shortest_visited[end], low_shortest_visited[end])
 
 def num_shortest(roadmap, elevations, start, end):
     """
     Returns the number of shortest highroad-lowroad paths
     The input is the graph of cities, a dictionary mapping cities to elevations, the start node and the end node.
     """
-    return 0
+    return algorithm(roadmap, elevations, start, end)[1]
 
-def num_shortest_helper(roadmap, elevations, start, end, current):
-    if low is end and high is end:
-        return 1
-    
+def algorithm(roadmap, elevations, start, end):    
+    num_ways = {} # will represent the number of ways to get to an outcome
+    moves = {} # will represent the number of moves to reach a node
+
+    for node in roadmap.get_nodes():
+        for node2 in roadmap.get_nodes():
+            moves[node+node2] = inf # we want min number of moves, so set initially to infinity
+            num_ways[node+node2] = -inf # we want max number of moves, set initially to -inifity
+
+    moves[start+start] = 0 # 0 moves from start to start
+    num_ways[start+start] = 1 # 1 way to get from start to start
+
+    q = queue.Queue() # create queue and enqueue the starting pair
+    q.put((start, start))
+
+    while not q.empty():
+        # grab the top pair on the stack
+        low, high = q.get()
+
+        current_moves = moves[low+high] # get the current values from the moves and num ways dictionaries
+        current_num_ways = num_ways[low+high]
+
+        # can we move the high node?
+        for adj in roadmap.get_neighbors(high):
+            # is it a valid move?
+            if elevations[adj] > elevations[low]:
+                # have we solved this / does it make sense to continue
+                if current_moves + 1 < moves[low+adj]: # we have a new best way to get to a node
+                    moves[low+adj] = current_moves + 1 # reflect the number of moves and ways to reflect
+                    num_ways[low+adj] = current_num_ways
+                    q.put((low, adj)) # enqueue this pair, we need to solve again
+                elif current_moves + 1 == moves[low+adj]: # we have found more ways to get the same outcome, update numways to reflect
+                    num_ways[low+adj] += current_num_ways
+            elif adj == low and adj == end: # so we have not found an eligble node, unless the node we are attempting to is the end node, in which case the usual rules of moving do not apply, we can end up at the same node
+                if current_moves + 1 < moves[end+end]: # we have found a new best way to reach the end node, update numways and moves to reflect
+                    num_ways[end+end] = current_num_ways
+                    moves[end+end] = current_moves + 1
+                elif current_moves + 1 == moves[end+end]: # we have found an equally good way to find the end node, update numways to reflect
+                    num_ways[end+end] += current_num_ways
+
+        # can we move the low node?
+        for adj in roadmap.get_neighbors(low):
+            # is it a valid move?
+            if elevations[adj] < elevations[high]:
+                # have we solved this / does it make sense to continue
+                if current_moves + 1 < moves[adj+high]:
+                    moves[adj+high] = current_moves + 1
+                    num_ways[adj+high] = current_num_ways
+                    q.put((adj, high))
+                elif current_moves + 1 == moves[adj+high]:
+                    num_ways[adj+high] += current_num_ways
+            elif adj == high and adj == end:
+                if current_moves + 1 < moves[end+end]:
+                    num_ways[end+end] = current_num_ways
+                    moves[end+end] = current_moves + 1
+                elif current_moves + 1 == moves[end+end]:
+                    num_ways[end+end] += current_num_ways
+
+        # can we move both low and high?
+        for adj_high in roadmap.get_neighbors(high):
+            for adj_low in roadmap.get_neighbors(low):
+                if elevations[adj_high] > elevations[adj_low]:
+                    if current_moves + 1 < moves[adj_low+adj_high]:
+                        moves[adj_low+adj_high] = current_moves + 1
+                        num_ways[adj_low+adj_high] = current_num_ways
+                        q.put((adj_low, adj_high))
+                    elif current_moves + 1 == moves[adj_low+adj_high]:
+                        num_ways[adj_low+adj_high] += current_num_ways
+                elif adj_high == adj_low and adj_high == end:
+                    if current_moves + 1 < moves[end+end]:
+                        num_ways[end+end] = current_num_ways
+                        moves[end+end] = current_moves + 1
+                    elif current_moves + 1 == moves[end+end]:
+                        num_ways[end+end] += current_num_ways
+    return moves[end+end], num_ways[end+end]
--- graphs/highroad_lowroad_tester.py
+++ graphs/highroad_lowroad_tester.py
@@ -2,7 +2,8 @@ from graph_util import *
 from highroad_lowroad import *
 import os
 
-tests = ['test1.txt', 'test2.txt', 'exponential_length5.txt', 'exponential_length10.txt', 'exponential_length30.txt', 'plateau.txt', 'four_layers10.txt', 'four_layers50.txt']
+tests = ['mountain.txt', 'branches.txt', 'test1.txt', 'test2.txt', 'exponential_length5.txt', 'exponential_length10.txt', 'exponential_length30.txt', 'plateau.txt', 'four_layers10.txt', 'four_layers50.txt']
+#tests = ['test1.txt']
 
 def read_input(filename):
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
